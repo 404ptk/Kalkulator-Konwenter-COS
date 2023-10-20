d126da195b8929eda6b410286e490f574ab5c49f
404ptk <108901659+404ptk@users.noreply.github.com>
2023-10-20 14:15:45 +0200

    Add files via upload

--- src/menu.java
+++ src/menu.java
@@ -1,10 +1,39 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class menu {
+public class menu extends JFrame{
     private JPanel JPanel1;
-    private JTextField aplikacjaPrzedstawiającaPrzykładoweWykorzystanieTextField;
     private JButton kalkulatorButton;
-    private JButton cosButton;
+    private JButton a3Button;
     private JButton conventerButton;
     private JButton wyjścieButton;
+
+    public static void main(String[] args) {
+        menu Menu = new menu();
+        Menu.setVisible(true);
+    }
+
+    public menu(){
+        super("Moje pierwsze GUI");
+        this.setContentPane(this.JPanel1);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int width = 800, height = 300;
+        this.setSize(width, height);
+
+        wyjścieButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        kalkulatorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
 }
