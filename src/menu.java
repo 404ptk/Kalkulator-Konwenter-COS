889cd50bb90d22d824cd91bec2ed8142f75d1ee2
404ptk <108901659+404ptk@users.noreply.github.com>
2023-10-20 13:01:15 +0200

    Add files via upload

---
+++ src/menu.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class menu {
+    private JPanel JPanel1;
+    private JTextField aplikacjaPrzedstawiającaPrzykładoweWykorzystanieTextField;
+    private JButton kalkulatorButton;
+    private JButton cosButton;
+    private JButton conventerButton;
+    private JButton wyjścieButton;
+}
